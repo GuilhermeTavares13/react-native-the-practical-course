e74c1a559d75854f5d0e651d55d6768aac04dd48
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-07-11 04:37:25 -0300

    Update files

--- RNCourse2/screens/GameOverScreen.js
+++ RNCourse2/screens/GameOverScreen.js
@@ -1,7 +1,38 @@
-import {View} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors'; 
 
 function GameOverScreen() {
-    return <View></View>
+    return (
+    <View>
+        <Title>GAME OVER!</Title>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../assets/images/success.png')}/>
+        </View> 
+    </View>
+    )
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center'
+    },
+    imageContainer: {
+   
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
